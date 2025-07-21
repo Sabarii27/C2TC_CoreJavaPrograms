f3285a4dd5592a84d6d35038e2d0146974950a50
sabari <muthusabari2710@gmail.com>
2025-07-21 12:25:34 +0530

    add new line

--- C2TC_CoreJavaPrograms/src/Day1/Basics/SampleDemo.java
+++ C2TC_CoreJavaPrograms/src/Day1/Basics/SampleDemo.java
@@ -5,6 +5,7 @@ public class SampleDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to C2TC.. ");
+		System.out.println("hiiiiii ");
 
 	}
 
