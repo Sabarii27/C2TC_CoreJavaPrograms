ebde4ccdfa033274812898287e4e2f467138f337
sabari <muthusabari2710@gmail.com>
2025-07-23 23:54:25 +0530

    1

---
+++ C2TC_CoreJavaPrograms/src/Day2/Basic/CustomerDemo.java
@@ -0,0 +1,15 @@
+package Day2.Basic;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Customer c1=new Customer();
+		System.out.println(c1);
+		
+		Customer c2=new Customer(102,"jackk","chennai");
+		System.out.println(c2);
+
+	}
+
+}
---
+++ C2TC_CoreJavaPrograms/src/DayOne/IdentifersDemo.java
@@ -0,0 +1,15 @@
+package DayOne;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $num=20;
+		System.out.println("value of the number variable is :"+ $num);
+		
+		String StdName="sabari";
+		System.out.println("value :"+ StdName);
+
+	}
+
+}
