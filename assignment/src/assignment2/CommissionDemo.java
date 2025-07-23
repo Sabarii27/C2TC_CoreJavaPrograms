9a3e3d0edfa59801eddbd780edc698d202486e09
sabari <muthusabari2710@gmail.com>
2025-07-23 22:23:00 +0530

    hj

---
+++ assignment/src/assignment2/CommissionDemo.java
@@ -0,0 +1,13 @@
+package assignment2;
+
+public class CommissionDemo {
+	public static void main(String[] args) {
+		
+        Commission obj = new Commission();
+        obj.acceptDetails();
+        obj.calculateCommission();
+        
+        
+    }
+
+}
