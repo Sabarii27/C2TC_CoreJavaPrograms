85694e6ed536bccfdc2d5698ac336400986f2424
sabari <muthusabari2710@gmail.com>
2025-07-24 13:51:06 +0530

    Day4 ThirdPackage

--- C2TC_CoreJavaPrograms/src/DayFour/SecondPackage/Executor.java
+++ C2TC_CoreJavaPrograms/src/DayFour/SecondPackage/Executor.java
@@ -1,19 +1,12 @@
 //Program to demonstrate accessing classes and methods of different package
-package com.tnsif.dayfour.secondpackage;
+package DayFour.SecondPackage;
 
-import com.tnsif.dayfour.firstpackage.Base;
+
 
 public class Executor {
 	public static void main(String[] args) {
 		// accessing different package class
-		Base b1 = new Base();
-
-		// private, default and protected members can't access
-
-		/*
-		 * b1.varPrivate=21; b1.methodPrivate(); b1.varDefault=11; b1.methodDefault();
-		 * b1.varProtected=31; b1.methodProtected();
-		 */
+		DayFour.FirstPackage.Base b1 = new DayFour.FirstPackage.Base();
 
 		b1.methodPublic();
 		b1.varPublic = 41;
