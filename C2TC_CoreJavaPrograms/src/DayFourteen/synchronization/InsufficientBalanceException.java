5a4b8a4a33aa15103c68f77e04435a3a2aa3552e
sabari <muthusabari2710@gmail.com>
2025-08-01 16:24:15 +0530

    day 14 package

---
+++ C2TC_CoreJavaPrograms/src/DayFourteen/synchronization/InsufficientBalanceException.java
@@ -0,0 +1,12 @@
+package DayFourteen.synchronization;
+
+public class InsufficientBalanceException extends Exception {
+
+	public InsufficientBalanceException() {
+		super("Insufficient balance in your account");
+	}
+
+	public InsufficientBalanceException(String message) {
+		super(message);
+	}
+}
