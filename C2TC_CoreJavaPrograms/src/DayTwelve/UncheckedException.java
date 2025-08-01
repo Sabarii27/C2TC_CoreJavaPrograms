6e29efaf9d62c27440b8c5028be5a4e4456b1ec3
sabari <muthusabari2710@gmail.com>
2025-08-01 16:04:05 +0530

    Day 12 package

---
+++ C2TC_CoreJavaPrograms/src/DayTwelve/UncheckedException.java
@@ -0,0 +1,10 @@
+package DayTwelve;
+
+public class UncheckedException {
+	public static void main(String[] args) {
+		int intArray[];
+		//try {
+			intArray = new int[] { 1, 2, 3, 4 };
+			System.out.println(intArray[6]);
+			}
+}
